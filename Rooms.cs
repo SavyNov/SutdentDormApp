71570c3aa3e4b77aa11fad864fcb33805c960c2e
SavyNov <savynovv@gmail.com>
2023-03-06 11:46:06 +0200

    Database created

---
+++ Rooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Rooms
+    {
+        public string ID { get; set; }
+        public string RoomNumber { get; set; }
+    }
+}
